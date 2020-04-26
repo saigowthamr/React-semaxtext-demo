93a9c3b266e501ab58c0eadbc29c08d8b215acad
saigowthamr <saigowthamr@gmail.com>
2020-04-26 19:40:52 +0530

    first commit

---
+++ src/components/Notfound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NotFound() {
+    return <h1>(404) == NotFound Page</h1>
+}
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Switch, NavLink } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import NotFound from './components/Notfound';
+
+import { createBrowserHistory as createHistory } from 'history';
+
+const history = createHistory();
+
+history.listen((location, action) => {
+  if (action !== 'REPLACE') {
+    window.strum('routeChange', window.location.href);
+  }
+})
+
+
+const Routing = (
+  <Router history={history}>
+    <div>
+      <ul>
+        <li>
+          <NavLink exact activeClassName="active" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/contact">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  Routing,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
